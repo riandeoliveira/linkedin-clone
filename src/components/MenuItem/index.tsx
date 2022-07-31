efc96b96ca58bee807a27e377a64ac60ef7739a9
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-07-31 00:21:31 -0300

    feat: create MenuItem component

---
+++ src/components/MenuItem/index.tsx
@@ -0,0 +1,20 @@
+import type { IMenuItem } from "interfaces";
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
+interface MenuItemProps extends IMenuItem {}
+
+const MenuItem = ({ icon, path, title }: MenuItemProps): JSX.Element => {
+  return (
+    <li className={`${styles.item} ${path === "/work" && styles.bar}`}>
+      <Link href={path}>
+        <a className={styles.link}>
+          <div className={styles.icon}>{icon}</div>
+          <span>{title}</span>
+        </a>
+      </Link>
+    </li>
+  );
+};
+
+export default MenuItem;
