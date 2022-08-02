41dc8ffa45fd29b73fcade107d6fcaee78e898f9
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-02 11:18:16 -0300

    feat: create global interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -9,3 +9,20 @@ export interface IUserSuggestion {
   career: string;
   fullname: string;
 }
+
+export interface IUserPost {
+  comments: number;
+  reactions: number;
+}
+
+export interface IFeaturedPost extends IUserPost {
+  content: string;
+  post: string;
+  title: string;
+}
+
+export interface IActivityPost extends IUserPost {
+  author: string;
+  content: string;
+  date: string;
+}
