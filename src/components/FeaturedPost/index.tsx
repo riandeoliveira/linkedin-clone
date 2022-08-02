845899e7d90c610f797d71d5b7ed03c178061beb
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-02 11:19:31 -0300

    feat: create FeaturedPost component

---
+++ src/components/FeaturedPost/index.tsx
@@ -0,0 +1,34 @@
+import { PostFooter } from "components";
+import type { IFeaturedPost } from "interfaces";
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+interface FeaturedPostProps extends IFeaturedPost {}
+
+const FeaturedPost = ({
+  title,
+  content,
+  post,
+  reactions,
+  comments,
+}: FeaturedPostProps): JSX.Element => {
+  return (
+    <div className={styles.post}>
+      <div className={styles.heading}>
+        <span className={styles.title}>{title}</span>
+        <span className={styles.content}>{content}</span>
+      </div>
+      <Image
+        src={`/assets/icons/${post}.svg`}
+        alt="Imagem da postagem do usuário"
+        width={236}
+        height={152}
+      />
+      <div className={styles.footer}>
+        <PostFooter reactions={reactions} comments={comments} />
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedPost;
