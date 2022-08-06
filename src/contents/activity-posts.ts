b7fb8c3a5e28a1b2f132a8d58567629f95a9baca
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-06 11:05:34 -0300

    refactor: update contents

--- src/contents/activity-posts.ts
+++ src/contents/activity-posts.ts
@@ -5,7 +5,7 @@ const activityPosts: IActivityPost[] = [
     author: "Nome Sobrenome",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu...",
-    post: "post-1",
+    post: "post-4",
     date: "1 sem",
     comments: 5,
     reactions: 124,
@@ -14,7 +14,7 @@ const activityPosts: IActivityPost[] = [
     author: "Nome Sobrenome",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu...",
-    post: "post-2",
+    post: "post-5",
     date: "1 sem",
     comments: 5,
     reactions: 124,
--- src/contents/index.ts
+++ src/contents/index.ts
@@ -1,6 +1,13 @@
+import activityPosts from "./activity-posts";
 import featuredPosts from "./featured-posts";
 import menuItems from "./menu-items";
 import suggestedConnections from "./suggested-connections";
 import suggestedPeople from "./suggested-people";
 
-export { featuredPosts, menuItems, suggestedConnections, suggestedPeople };
+export {
+  activityPosts,
+  featuredPosts,
+  menuItems,
+  suggestedConnections,
+  suggestedPeople,
+};
