4c40d415bda2029d0d44b3834278175c252eb870
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-06 17:06:25 -0300

    feat: create user languages content

---
+++ src/contents/user-languages.ts
@@ -0,0 +1,14 @@
+import type { IUserLanguage } from "interfaces";
+
+const userLanguages: IUserLanguage[] = [
+  {
+    language: "Português",
+    level: "Fluente ou nativo",
+  },
+  {
+    language: "Inglês",
+    level: "Intermediário",
+  },
+];
+
+export default userLanguages;
