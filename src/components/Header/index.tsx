b753abbdc318a10b787c67473c2d49444f2e6595
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-08-07 21:45:38 -0300

    feat: add Header responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,36 +7,38 @@ const Header = (): JSX.Element => {
   return (
     <header className={styles.header}>
       <div className={styles.header_container}>
-        <div className={styles.heading}>
-          <Image
-            src="/assets/icons/linkedin.svg"
-            alt="Logo branca do LinkedIn"
-            width={34}
-            height={34}
-          />
-          <div className={styles.input_container}>
-            <div className={styles.search_icon}>
-              <Image
-                src="/assets/icons/search.svg"
-                alt=""
-                width={17.5}
-                height={17.5}
+        <div className={styles.container}>
+          <div className={styles.heading}>
+            <Image
+              src="/assets/icons/linkedin.svg"
+              alt="Logo branca do LinkedIn"
+              width={34}
+              height={34}
+            />
+            <div className={styles.input_container}>
+              <div className={styles.search_icon}>
+                <Image
+                  src="/assets/icons/search.svg"
+                  alt=""
+                  width={17.5}
+                  height={17.5}
+                />
+              </div>
+              <input
+                type="text"
+                placeholder="Pesquisar"
+                className={styles.input}
               />
             </div>
-            <input
-              type="text"
-              placeholder="Pesquisar"
-              className={styles.input}
-            />
           </div>
+          <nav className={styles.nav}>
+            <ul className={styles.list}>
+              {menuItems.map(({ title, path, icon }, i) => (
+                <MenuItem title={title} path={path} icon={icon} key={i} />
+              ))}
+            </ul>
+          </nav>
         </div>
-        <nav className={styles.nav}>
-          <ul className={styles.list}>
-            {menuItems.map(({ title, path, icon }, i) => (
-              <MenuItem title={title} path={path} icon={icon} key={i} />
-            ))}
-          </ul>
-        </nav>
       </div>
     </header>
   );
